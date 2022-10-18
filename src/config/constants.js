13f09c85aef1a44e7bdb2bc7fefe820183d99c73
MinnieNam <gertrude0726@naver.com>
2022-10-18 16:36:46 +0900

    feat: Game 컴포넌트 Layout, App 컴포넌트에 연결 및 constants 셋팅

---
+++ src/config/constants.js
@@ -0,0 +1,18 @@
+export const LOGO = {
+  C: "C",
+  O: "o",
+  BLOCKS: "blocks",
+};
+
+export const BUTTON = {
+  TUTORIAL: "연습하기",
+  GAME_SELECTION: "게임선택",
+  REPEAT: "다시하기",
+  NEXT_GAME: "다음게임",
+  START: "시작하기",
+};
+
+export const WINDOW = {
+  BLOCKS_SELECTION: "코드 블록 선택하기",
+  BLOCKS_LOGIC: "코드 블록 놓기",
+};
---
+++ src/pages/Game/index.js
@@ -0,0 +1,80 @@
+import React from "react";
+import styled from "styled-components";
+
+import Header from "../../components/Header/index";
+import BlockCombinator from "../../components/BlockCombinator/index";
+import Map from "../../components/Map/index";
+
+import { BUTTON } from "../../config/constants";
+
+function Game() {
+  return (
+    <>
+      <Header />
+      <TopWrapper>
+        <Title>여기는 제목 자리입니다.</Title>
+        <ButtonsWrapper>
+          <Button rightMargin={"4vw"}>{BUTTON.REPEAT}</Button>
+          <Button rightMargin={"15vw"}>{BUTTON.NEXT_GAME}</Button>
+        </ButtonsWrapper>
+      </TopWrapper>
+      <BottomWrapper>
+        <BlockCombinator />
+        <RightWrapper>
+          <Map />
+          <Button>{BUTTON.START}</Button>
+        </RightWrapper>
+      </BottomWrapper>
+    </>
+  );
+}
+
+const TopWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 5vw;
+`;
+
+const Title = styled.div`
+  margin-left: 15vw;
+  font-size: 1.5vw;
+  font-weight: bolder;
+`;
+
+const ButtonsWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const Button = styled.button`
+  display: flex;
+  align-items: center;
+  height: 3.5vw;
+  padding: 1vw;
+  margin-right: ${(props) => props.rightMargin};
+  border: 0.3vw solid #000000;
+  border-radius: 2vw;
+  background: #e1b2c6;
+  color: #000000;
+  font-size: 1.3vw;
+  font-weight: bolder;
+  box-shadow: 0vw 0.3vw 0.3vw rgba(0, 0, 0, 0.25);
+`;
+
+const BottomWrapper = styled.div`
+  display: flex;
+  align-items: start;
+  justify-content: space-around;
+  margin: 0 15vw;
+`;
+
+const RightWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: column;
+  height: 70vh;
+`;
+
+export default Game;
