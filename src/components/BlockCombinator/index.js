b7b23131d5c29fdb8e1e76c319fa3314210fa9ae
ggooltae <waterboy94@naver.com>
2022-10-19 02:11:27 +0900

    feat: 블록 드래그&드랍 element 생성기능 구현

--- src/components/BlockCombinator/index.js
+++ src/components/BlockCombinator/index.js
@@ -1,24 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 import { WINDOW } from "../../config/constants";
 
 function BlockCombinator() {
-  function BlockSelectionWindow() {
-    return <Title color={"#ffffff"}>{WINDOW.BLOCKS_SELECTION}</Title>;
-  }
+  const blocks = [
+    "1칸전진",
+    "왼쪽회전",
+    "오른쪽회전",
+    "if",
+    "while",
+    "공격",
+    "놓기",
+  ];
 
-  function BlockLogicWindow() {
-    return <Title color={"#000000"}>{WINDOW.BLOCKS_LOGIC}</Title>;
-  }
+  const [logicBlocks, setLogicBlocks] = useState(
+    Array.from({ length: 10 }).fill(""),
+  );
+
+  const dragStart = (event) => {
+    event.dataTransfer.setData("text", event.target.innerText);
+  };
+
+  const drop = (event) => {
+    const data = event.dataTransfer.getData("text");
+
+    if (data && logicBlocks.includes("")) {
+      const newLogicBlocks = logicBlocks.slice();
+
+      newLogicBlocks.splice(newLogicBlocks.indexOf(""), 1, data);
+      setLogicBlocks(newLogicBlocks);
+    }
+  };
+
+  const allowDrop = (event) => {
+    event.preventDefault();
+  };
 
   return (
     <Wrapper>
       <WindowWrapper backGroundColor={"#64a9bd"}>
-        <BlockSelectionWindow />
+        <Title color={"#ffffff"}>{WINDOW.BLOCKS_SELECTION}</Title>
+        {blocks.map((blockTitle) => (
+          <Block
+            key={blockTitle}
+            id={blockTitle}
+            onDragStart={dragStart}
+            draggable="true"
+          >
+            {blockTitle}
+          </Block>
+        ))}
       </WindowWrapper>
-      <WindowWrapper backGroundColor={"#f5ed58"}>
-        <BlockLogicWindow />
+      <WindowWrapper
+        backGroundColor={"#f5ed58"}
+        onDrop={drop}
+        onDragOver={allowDrop}
+      >
+        <Title color={"#000000"} draggable="false">
+          {WINDOW.BLOCKS_LOGIC}
+        </Title>
+        {logicBlocks.map((blockTitle, index) =>
+          blockTitle ? (
+            <Block
+              key={`${blockTitle}${index}`}
+              id={`${blockTitle}${index}`}
+              draggable="true"
+            >
+              {blockTitle}
+            </Block>
+          ) : (
+            <EmptyBlock
+              key={`${blockTitle}${index}`}
+              id={`${blockTitle}${index}`}
+            >
+              {blockTitle}
+            </EmptyBlock>
+          ),
+        )}
       </WindowWrapper>
     </Wrapper>
   );
@@ -37,12 +96,41 @@ const Wrapper = styled.div`
 `;
 
 const WindowWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   width: 20vw;
   height: 70vh;
   margin-right: 3vw;
-  border: 0.3vw solid #000000;
+  border: 3px solid #000000;
   background-color: ${(props) => props.backGroundColor};
   box-shadow: 0vw 0.3vw 0.3vw rgba(0, 0, 0, 0.25);
+`;
+
+const Block = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 70%;
+  height: 2.5rem;
+  margin: 0.2rem 0;
+  border: 3px solid #000000;
+  background-color: #7e72ce;
+  color: white;
+  font-weight: bolder;
+`;
+
+const EmptyBlock = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 70%;
+  height: 2.5rem;
+  margin: 0.2rem 0;
+  border: 2px dashed #000000;
+  background-color: transparent;
+  color: white;
+  font-weight: bolder;
 `;
 
 export default BlockCombinator;
