04ce8787394217003ed2a630cd4a50bcad6ca412
ggooltae <waterboy94@naver.com>
2022-10-22 02:39:03 +0900

    feat: 캐릭터 이동 애니메이션 및 제자리걸음 구현

--- src/config/constants.js
+++ src/config/constants.js
@@ -21,3 +21,24 @@ export const WINDOW = {
   BLOCKS_SELECTION: "코드 블록 선택하기",
   BLOCKS_LOGIC: "코드 블록 놓기",
 };
+
+export const CHARACTER_DIRECTION = {
+  UP: 0,
+  RIGHT: 1,
+  DOWN: 2,
+  LEFT: 3,
+  STAY: 999,
+};
+
+export const ASSET = {
+  SINGLE_ASSET_WIDTH: 46,
+  SINGLE_ASSET_HEIGHT: 46,
+  ROCK: 20,
+  WATER: 21,
+  CAT_SPRITE_FRAMES: 4,
+};
+
+export const RENDER = {
+  SINGLE_TILE_WIDTH: 40,
+  SINGLE_TILE_HEIGHT: 40,
+};
