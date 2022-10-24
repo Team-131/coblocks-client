329d412bedb3a2ba1b9a0720d9d436c20883d903
jun2212 <hsj890509@gmail.com>
2022-10-24 18:20:20 +0900

    feat: blockSlice 기능추가

--- src/features/block/blockSlice.js
+++ src/features/block/blockSlice.js
@@ -4,6 +4,7 @@ const blockSlice = createSlice({
   name: "blockSlice",
   initialState: {
     translatedBlocks: [],
+    isStarted: false,
   },
   reducers: {
     resetTranslatedBlocks: (state, action) => {
@@ -12,9 +13,12 @@ const blockSlice = createSlice({
     updateTranslatedBlocks: (state, action) => {
       state.translatedBlocks = action.payload.translatedBlocks;
     },
+    setIsStarted: (state, action) => {
+      state.isStarted = action.payload.isStarted;
+    },
   },
 });
 
 export { blockSlice };
-export const { resetTranslatedBlocks, updateTranslatedBlocks } =
+export const { resetTranslatedBlocks, updateTranslatedBlocks, setIsStarted } =
   blockSlice.actions;
