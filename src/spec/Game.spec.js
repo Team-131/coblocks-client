61b8efea1018cc0c0ec79ab5af85df1d7e033af1
ggooltae <waterboy94@naver.com>
2022-11-01 22:50:42 +0900

    test: Game 컴포넌트 unit test 작성

---
+++ src/spec/Game.spec.js
@@ -0,0 +1,24 @@
+import { screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+import { renderWithProvider } from "./test-utils";
+
+describe("Game 컴포넌트", () => {
+  it("다음게임 버튼 클릭시 다음 스테이지로 이동", async () => {
+    const user = userEvent.setup();
+
+    renderWithProvider(["/game/stage1"]);
+
+    await user.click(screen.getByText("다음게임"));
+    expect(screen.getByText(/나무/i)).toBeInTheDocument();
+
+    await user.click(screen.getByText("다음게임"));
+    expect(screen.getByText(/사막 탈출/i)).toBeInTheDocument();
+
+    await user.click(screen.getByText("다음게임"));
+    expect(screen.getByText(/바람개비/i)).toBeInTheDocument();
+
+    await user.click(screen.getByText("다음게임"));
+    expect(screen.getByText(/꼬불꼬불/i)).toBeInTheDocument();
+  });
+});
