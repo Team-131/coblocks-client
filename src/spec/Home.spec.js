0d0d7ff7d069628050da6da040077533f4091a47
ggooltae <waterboy94@naver.com>
2022-11-01 22:50:13 +0900

    test: Home 컴포넌트 unit test 작성

---
+++ src/spec/Home.spec.js
@@ -0,0 +1,36 @@
+import { screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+import { renderWithProvider } from "./test-utils";
+
+describe("Home 컴포넌트", () => {
+  it("Home 컴포넌트 렌더링시 element 렌더 정상여부체크", () => {
+    renderWithProvider(["/"]);
+
+    expect(screen.getByText(/연습하기/i)).toBeInTheDocument();
+    expect(screen.getByText(/게임선택/i)).toBeInTheDocument();
+  });
+
+  it("연습하기 버튼 클릭시 연습하기 페이지로 이동", async () => {
+    renderWithProvider(["/"]);
+
+    const user = userEvent.setup();
+
+    await user.click(screen.getByText("연습하기"));
+
+    expect(screen.getByText(/코드 블록 선택하기/i)).toBeInTheDocument();
+    expect(screen.getByText(/어느 방향일까?/i)).toBeInTheDocument();
+  });
+
+  it("게임하기 버튼 클릭시 게임리스트 페이지로 이동", async () => {
+    renderWithProvider(["/"]);
+
+    const user = userEvent.setup();
+
+    await user.click(screen.getByText("게임선택"));
+
+    expect(
+      screen.getByText(/도전하고 싶은 맵을 선택하세요./i),
+    ).toBeInTheDocument();
+  });
+});
