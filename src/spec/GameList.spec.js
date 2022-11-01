abe49e18b5d7b16dbae62425a7da4474e3999f48
ggooltae <waterboy94@naver.com>
2022-11-01 22:50:29 +0900

    test: GameList 컴포넌트 unit test 작성

---
+++ src/spec/GameList.spec.js
@@ -0,0 +1,33 @@
+import { screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+import { renderWithProvider } from "./test-utils";
+
+describe("GameList 컴포넌트", () => {
+  it("GameList 컴포넌트 렌더링시 element 렌더 정상여부체크", () => {
+    renderWithProvider(["/game_list"]);
+
+    expect(
+      screen.getByText(/도전하고 싶은 맵을 선택하세요./i),
+    ).toBeInTheDocument();
+    expect(screen.getByText(/stage1/i)).toBeInTheDocument();
+  });
+
+  it("맵 클릭시 게임 페이지로 이동", async () => {
+    renderWithProvider(["/game_list"]);
+
+    const user = userEvent.setup();
+
+    await user.click(screen.getByText(/stage1/i));
+
+    expect(screen.getByText(/코드 블록 선택하기/i)).toBeInTheDocument();
+    expect(screen.getByText(/코드 블록 놓기/i)).toBeInTheDocument();
+    expect(screen.getByText(/앞으로 1칸 이동/i)).toBeInTheDocument();
+    expect(screen.getByText(/오른쪽으로 회전하기/i)).toBeInTheDocument();
+    expect(screen.getByText(/왼쪽으로 회전하기/i)).toBeInTheDocument();
+    expect(screen.getByText(/공격하기/i)).toBeInTheDocument();
+    expect(screen.getByText(/만약/i)).toBeInTheDocument();
+    expect(screen.getByText(/계속 반복하기/i)).toBeInTheDocument();
+    expect(screen.getByText("🔁 반복하기")).toBeInTheDocument();
+  });
+});
